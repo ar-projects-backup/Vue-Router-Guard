f97f1f96afb61ff37cd3ef497439c892f7b15495
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2020-05-29 16:20:05 -0400

    Add - vue router guard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,54 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Login from './components/Login.vue'
+import Secure from './components/Secure.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex);
+Vue.use(VueRouter);
+
+const store = new Vuex.Store({
+  state: {
+    authenticated: false
+  },
+  mutations: {
+    setAuthentication(state, status) {
+      this.state.authenticated = status;
+    }
+  }
+});
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/secure',
+      name: 'secure',
+      component: Secure,
+      beforeEnter: (to, from, next) => {
+        if(store.state.authenticated == false) {
+            next('login');
+        } else {
+            next();
+        }
+      }      
+    }
+  ]
+});
+
 new Vue({
   render: h => h(App),
+  store: store,
+  router: router
 }).$mount('#app')
