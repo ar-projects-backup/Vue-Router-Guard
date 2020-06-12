5b0f706532ce14e57dbcfc353c180cdaa786b578
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2020-06-12 19:15:48 -0400

    add | Demonstração de como utilizar Vue-Router-Guard

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <form>
+      <input  type="text"   name="user"      v-model="input.username" placeholder="User Name">
+      <input  type="text"   name="password"  v-model="input.password" placeholder="Password">
+      <button type="submit" class="btn blue" @click.prevent="login()">Enviar</button>
+    </form>        
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      input: {
+        username : '',
+        password : ''
+      }
+    }
+  },
+  mounted() {
+    
+  },
+  methods: {
+    login() {
+      if ((this.input.username == 'admin') && (this.input.password == 'password')) {
+        this.$store.commit('setAuthentication', true);
+        this.$router.replace({ name: 'secure' });
+      } else {
+        alert('Usuário ou senha incorreta');
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Secure.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Secure Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Secure'  
+}
+</script>
+
+<style scoped>
+
+</style>>
